f73a5c7398bd695cd2a999caa47d81fbad8c3315
JaredPC <jaredpc12@gmail.com>
2019-02-13 17:22:53 -0800

    Distance Sensor Test

--- test_hardware.c
+++ test_hardware.c
@@ -13,10 +13,12 @@
 #define PHOTO_DIODE_LEFT 1
 #define LINE_SENSOR_RIGHT 3
 #define LINE_SENSOR_LEFT 2
+#define DISTANCE_SENSOR 5
 
 void poll_linesensor(u08 pin_num);
 void poll_photodiode(u08 pin_num);
 void print_data(u08 count, u08 vals);
+void poll_distance(u08 pin_num);
 
 int main(void)
 {
@@ -24,11 +26,11 @@ int main(void)
     motor(0, 0);
     motor(1, 0);
     
-    int _error[HISTORY_LENGTH] = {0};	
-	struct pid _pid = {_error, P_TERM, I_TERM, D_TERM};
-	int _sensors[2] = {0,0};
-	int _motors[2] = {0,0};
-	motor_command_t motors;
+ //    int _error[HISTORY_LENGTH] = {0};	
+	// struct pid _pid = {_error, P_TERM, I_TERM, D_TERM};
+	// int _sensors[2] = {0,0};
+	// int _motors[2] = {0,0};
+	// motor_command_t motors;
 
 	while((get_btn() == 0) && (get_btn2() == 0)){
 		delay_ms(1);
@@ -43,19 +45,22 @@ int main(void)
 		// poll_linesensor(LINE_SENSOR_RIGHT);
 		// poll_linesensor(LINE_SENSOR_LEFT);
 		// delay_ms(500);
-		_sensors[1] = poll_analog_pin(LINE_SENSOR_RIGHT);
-		_sensors[0] = poll_analog_pin(LINE_SENSOR_LEFT);
+		// _sensors[1] = poll_analog_pin(LINE_SENSOR_RIGHT);
+		// _sensors[0] = poll_analog_pin(LINE_SENSOR_LEFT);
 
-		pid_control(&_pid, &_sensors[0], &_motors[0]);
+		// pid_control(&_pid, &_sensors[0], &_motors[0]);
 
-		motors.right = _motors[1];
-		motors.left = _motors[0];
+		// motors.right = _motors[1];
+		// motors.left = _motors[0];
 
-		print_4(_sensors[0], motors.left, _sensors[1], motors.right);
+		// print_4(_sensors[0], motors.left, _sensors[1], motors.right);
 
-		set_motors(motors);
+		// set_motors(motors);
 
-		delay_ms(ERROR_CHECK_DELAY);
+		// delay_ms(ERROR_CHECK_DELAY);
+
+		poll_distance(DISTANCE_SENSOR);
+		delay_ms(200);
 
 	}
 }
@@ -69,7 +74,12 @@ void poll_linesensor(u08 pin_num){
 }
 
 void print_data(u08 count, u08 vals){
-	//print_num(count);
+	clear_screen();
+	print_num(count);
 	print_string(":");
 	print_num(vals);
+}
+
+void poll_distance(u08 pin_num){
+	print_data(pin_num, analog(pin_num));
 }
