fa8e3372a2666eb264df5f96f9e7247163e4fe7d
JaredPC <jaredpc12@gmail.com>
2019-02-18 14:49:54 -0800

    Choose numbers

--- lab4_part2.c
+++ lab4_part2.c
@@ -18,6 +18,12 @@ void print_training2(int count);
 void print_training3(int count1, int count2);
 void print_training4(int count1, int count2);
 
+void read_accel(u08 *horizontal);
+void print_target_block(u08 num);
+void print_num_block(u08 num);
+u08 get_target_block(u08 num_block);
+u08 get_num_block();
+
 int main(void)
 {
     init();
@@ -25,6 +31,7 @@ int main(void)
     // Variables
     u08 num_block;
     u08 target_block;
+    
     line_data_t line_data;
     motor_command_t motors;
     motors.left = 0;
@@ -39,27 +46,85 @@ int main(void)
     while((get_btn() == 0) && (get_btn2() == 0)){
         delay_ms(1);
     }
+    //Button Press Delay before start
+    delay_ms(100);
 
     //Choose Number of Blocks
     num_block = get_num_block();
 
-    //Button Press Delay before start
-    delay_ms(200);
+    //Choose Target Block
+    target_block = get_target_block(num_block);
 
+    //Loop until finished
     while (1){
 
     }
 }
 
+//Choose Number of Blocks
 u08 get_num_block(){
     u08 block_num = 3;
+    u08 accel_data = 0;
     while((get_btn() == 0) && (get_btn2() == 0)){
-        
+        print_num_block(block_num);
+        read_accel(&accel_data);
+        if(accel_data < 245){
+            if(accel_data > 128){
+                if(block_num < 5){
+                    block_num++;
+                }
+            }else if(accel_data > 10){
+                if(block_num > 3){
+                    block_num--;
+                }
+            }
+        }
+        delay_ms(50);
     }
+    return block_num;
+}
+
+//Choose Target Blocks
+u08 get_target_block(u08 num_block){
+    u08 block_num = 1;
+    u08 accel_data = 0;
+    while((get_btn() == 0) && (get_btn2() == 0)){
+        print_target_block(block_num);
+        read_accel(&accel_data);
+        if(accel_data < 245){
+            if(accel_data > 128){
+                if(block_num < num_block){
+                    block_num++;
+                }
+            }else if(accel_data > 10){
+                if(block_num > 1){
+                    block_num--;
+                }
+            }
+        }
+        delay_ms(50);
+    }
+    return block_num;
 }
 
 void print_num_block(u08 num){
-    
+    clear_screen();
+    print_string("BlockNum");
+    lcd_cursor(0, 1);
+    print_num(num);
+}
+
+void print_target_block(u08 num){
+    clear_screen();
+    print_string("Target");
+    lcd_cursor(0, 1);
+    print_num(num);
+}
+
+void read_accel(u08 *horizontal)
+{
+    // Directions when looking at the screen
+    horizontal[0] = get_accel_y(); // Left and right
 }
 
 void print_data(line_data_t sensor, int count){
