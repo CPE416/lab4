db042d8fbd2327638b57736ca057f0d44f424be1
JaredPC <jaredpc12@gmail.com>
2019-02-19 19:37:32 -0800

    Move

--- monte.h
+++ monte.h
@@ -13,5 +13,39 @@ void localize(){
     
 }
 
+// Struct and Functions for the Trapezoid Probability Density Function
+typedef struct {
+	float a;
+	float b;
+	float c;
+	float d;
+} trap_prob_t;
+
+trap_prob_t init_trap_prob(float a, float b, float c, float d){
+	trap_prob_t trap;
+    trap.a = a;
+    trap.b = b;
+    trap.c = c;
+    trap.d = d;
+    return trap;
+}
+
+float calc_prob(trap_prob_t trap, float x){
+	float u = 2/(trap.d + trap.c - trap.b - trap.a);
+	if(x < trap.a){
+		return 0;
+	}else if(x < trap.b){
+		float slope = (x - trap.a)/(trap.b - trap.a);
+		return (u * slope);
+	}else if(x < trap.c){
+		return u;
+	}else if(x < trap.d){
+		float slope = (trap.d - x)/(trap.d - trap.c);
+		return (u * slope);
+	}else{
+		return 0;
+	}
+}
+
 
 #endif 
--- simulation.c
+++ simulation.c
@@ -5,41 +5,16 @@
 #include <stdio.h>
 
 // Custum delay lib
-#include "hardware.h"
-#include "line_follow_pid.h"
+//#include "hardware.h"
+//#include "line_follow_pid.h"
 #include "monte.h"
 #include "kill.h"
 
 #define PARTICLE_NUM 100
 #define STANDARD_DEVIATION 1
 
-typedef struct {
-	float a;
-	float b;
-	float c;
-	float d;
-} trap_prob_t;
-
-trap_prob_t init_trap_prob(float a, float b, float c, float d){
-	trap_prob_t trap;
-    trap.a = a;
-    trap.b = b;
-    trap.c = c;
-    trap.d = d;
-    return trap;
-}
-
-float calc_prob(trap_prob_t trap, float x){
-	float u = 2/(trap.d + trap.c - trap.b - trap.a);
-	if(x < trap.b){
-		float b
-	}else if(x < trap.c){
-
-	}else{
-
-	}
-}
-
 int main(void){
+	
+
 
 }
